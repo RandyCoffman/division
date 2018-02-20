dbc5cb3ed1724155078697b0464e5f3c7baf212a
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-20 14:06:15 -0500

    Added tests to check if everything was running as expected

---
+++ first_div_test_group_ex.rb
@@ -0,0 +1,28 @@
+require "minitest/autorun"
+require_relative "div.rb"
+
+class Divisiontest < Minitest::Test
+	# def division(x,y)
+	# 	x * y
+	# end
+
+	def test_1_equals_1		#This is made to pass so I can establish a base
+		assert_equal(1,1)
+	end
+
+	#def test_1_equals_5 	#This is made to fail to make sure my test fails since 1 != 2
+		# assert_equal(1,5)
+	#end
+
+	def test_10_div_0_is_bad
+		assert_nil(nil, division(10,0))
+	end
+
+	def test_100_div_10_is10
+		assert_equal(10, division(100,10))
+	end
+
+	def test_20_div_2_is_10
+		assert_equal(10, division(20,2))
+	end
+end
